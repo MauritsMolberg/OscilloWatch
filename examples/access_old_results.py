d884b6b20afa7e42ec8373791be998a194da0a35
MauritsMolberg <93150678+MauritsMolberg@users.noreply.github.com>
2024-03-18 13:08:29 +0100

    Include start and end index in read_from_pkl

--- examples/access_old_results.py
+++ examples/access_old_results.py
@@ -5,8 +5,9 @@ from methods.read_from_pkl import read_from_pkl
 
 
 if __name__ == "__main__":
-    seg_res_list = read_from_pkl("../results/.utfall P NO-SE.pkl")
-    for segment in seg_res_list[:4]:
+    seg_res_list = read_from_pkl("../results/.utfall P NO-SE.pkl", 0,4)
+
+    for segment in seg_res_list:
         plt.figure()
         plt.plot(segment.input_signal)
         segment.hht.emd.plot_emd_results(show=False)
