e1094efffe4301c6eafa0258cae214f47cd96c41
MauritsMolberg <93150678+MauritsMolberg@users.noreply.github.com>
2024-03-18 13:12:46 +0100

    Include start and end index in read_from_pkl

--- methods/read_from_pkl.py
+++ methods/read_from_pkl.py
@@ -1,14 +1,29 @@
 import pickle
 
 
-def read_from_pkl(file_path="results.pkl"):
+def read_from_pkl(file_path, start_index=None, end_index=None):
     try:
         with open(file_path, 'rb') as file:
-            loaded_segment_results = []
-            while True:
-                loaded_segment = pickle.load(file)
-                loaded_segment_results.append(loaded_segment)
-    except EOFError:
-        pass  # End of file reached
+            loaded_objects = []
+            current_index = 0
 
-    return loaded_segment_results
+            while True:
+                try:
+                    loaded_object = pickle.load(file)
+                except EOFError:
+                    break  # End of file reached
+
+                if (start_index is None or current_index >= start_index) and \
+                   (end_index is None or current_index <= end_index):
+                    loaded_objects.append(loaded_object)
+
+                current_index += 1
+
+    except FileNotFoundError:
+        print(f"Error: File '{file_path}' not found.")
+        return []
+    except Exception as e:
+        print(f"Error occurred while reading from '{file_path}': {e}")
+        return []
+
+    return loaded_objects
